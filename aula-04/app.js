9f7bd7e68cf6ecd1dde3fa8e759931cd9d17ed7d
samusilva310109-a11y <samusilva310109@gmail.com>
2026-02-20 16:40:32 -0300

    finalização da aula-04

---
+++ aula-04/app.js
@@ -0,0 +1,7 @@
+/**
+ * Objetivo: arquivo responsável somente pelas entradas e saídas de dados
+ * Data: 20/02/2026 
+ * 
+ * Autor: Samuel Silva Moreira Dos Santos
+ * Versão: 1.0
+ */
---
+++ aula-04/module/calculo.js
@@ -0,0 +1,40 @@
+/**********************************************************************************
+ * Objetivo: Arquivo responsável pelo processamento de cálculos matemáticos (Somar,
+ * Subtrair, Multiplicar, Dividir)
+ * 
+ * Data: 20/02/2026
+ * Versão: 1.0
+**********************************************************************************/
+//toLowerCase -> retorna uma string em minusculo
+//toUpperCase -> retotorna um string em maiusculo
+
+//exemplo de função anônima
+
+//função para calcular as 4 operações matemáticas
+const calcular = function(numero1, numero2, operador){
+    //Entrada de dados
+    let valor1 = Number(numero1)
+    let valor2 = Number(numero2)
+    let operadorMatematico = String(operador).toUpperCase()
+
+    let resultado = false
+
+    //Processamento
+    if(operadorMatematico == 'SOMAR'){
+        resultado = valor1 + valor2
+    }else if(operadorMatematico == 'SUBTRAIR'){
+        resultado = valor1 - valor2
+    }else if(operadorMatematico == 'DIVIDIR'){
+        if(valor2 != 0)
+            resultado = valor1 / valor2
+    }else if(operadorMatematico == "MULTIPLICAR"){
+        resultado = valor1 * valor2
+    }
+    
+    //Saída
+    return resultado
+}
+
+let result = calcular(2,2,'dividir')
+
+console.log(result)
